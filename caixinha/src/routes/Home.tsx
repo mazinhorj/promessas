d6828df6a3390a90a33ad23a6c024ae24a9d01aa
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2022-12-26 11:57:53 -0300

    btn login

--- caixinha/src/routes/Home.tsx
+++ caixinha/src/routes/Home.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-//import { VersiculoProps } from "../types/versiculo";
-
 import { FaCross } from "react-icons/fa";
 
+import { useNavigate } from "react-router-dom";
 
-import classes from "./Home.module.css";
+import classes_h from "./Home.module.css";
+import classes from "./Versiculos.module.css";
 import Login from "../components/Login";
 
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div className={classes.app_home}>
+    <div className={classes_h.app_home}>
       <h1>Caixinha de Promessas</h1>
       <h2>
         <FaCross />
       </h2>
       <Login />
+      {/* coloquei essa div abaixo pq o componente Login está com problemas */}
+      <div className={classes.btns}>
+        <button className={classes.btn} onClick={() => navigate("/versiculos")}>
+          Entrar
+        </button>
+      </div>
     </div>
   );
 };
