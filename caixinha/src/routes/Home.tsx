2508c97ceb4e45405635538c90b3f89d475f377d
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2022-12-26 12:07:18 -0300

    alter login

--- caixinha/src/routes/Home.tsx
+++ caixinha/src/routes/Home.tsx
@@ -2,23 +2,34 @@ import { FaCross } from "react-icons/fa";
 
 import { useNavigate } from "react-router-dom";
 
-import classes_h from "./Home.module.css";
-import classes from "./Versiculos.module.css";
+import classes from "./Home.module.css";
+import classes_l from "../components/Login.module.css";
+import classes_b from "./Versiculos.module.css";
 import Login from "../components/Login";
 
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className={classes_h.app_home}>
+    <div className={classes.app_home}>
       <h1>Caixinha de Promessas</h1>
       <h2>
         <FaCross />
       </h2>
       <Login />
-      {/* coloquei essa div abaixo pq o componente Login está com problemas */}
-      <div className={classes.btns}>
-        <button className={classes.btn} onClick={() => navigate("/versiculos")}>
+      {/* coloquei essas divs abaixo pq o componente Login está com problemas */}
+      <div className={classes_l.login}>
+        {/* <label htmlFor="name">Nome:</label> */}
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite seu nome"
+          required
+        />
+      </div>
+      <div className={classes_b.btns}>
+        <button className={classes_b.btn} onClick={() => navigate("/versiculos")}>
           Entrar
         </button>
       </div>
